2b893be0206e8e338886810e722c0f4d03ee6dd3
Erik Kers <erik.kers.ohrnell@gmail.com>
2018-01-06 16:40:28 +0100

    Add New Recipe view and view-model

--- src/pages/new-recipe.ts
+++ src/pages/new-recipe.ts
@@ -1,12 +1,34 @@
+import { Ingredient } from "./../resources/helpers/ingredient";
+import { Instruction } from "./../resources/helpers/instruction";
+import { Recipe } from "resources/helpers/recipe";
+
 export class NewRecipe {
-  title: string;
-  description: string;
-  selectedFiles: any;
-  time: string;
-  course: string;
-  category: string;
-  categories: string[];
-  ingredient: string;
-  ingredients: string[];
-  extra: string;
+  private title: string;
+  private description: string;
+  private time: string;
+  private date: Date;
+  private imagePath: string;
+  private course: string;
+  private tags: string[];
+  private extra: string;
+  private instruction: Instruction;
+  private ingredient: Ingredient;
+  private ingredientList: Ingredient[] = [];
+  private instructionList: Instruction[] = [];
+  private recipe: Recipe;
+
+  constructor() {
+    this.title = '',
+    this.description = '',
+    this.time = '',
+    this.date = new Date();
+    this.imagePath = '';
+    this.course = '';
+    this.tags = [];
+    this.extra = '';
+  }
+  
+  public saveRecipe(title, description, time, date, imagePath, course, tags, extra): void {
+    console.log(this.recipe);
+  }
 }
