db51e4291a1c6039774cc15d8aea5decaf8bd7eb
Erik Kers <erik.kers.ohrnell@gmail.com>
2018-01-06 16:40:45 +0100

    Fix incorrect routes

--- src/app.ts
+++ src/app.ts
@@ -9,8 +9,8 @@ export class App {
     config.title = 'Cutting Edge';
     config.map([
       { route: ['', 'home'], name: 'home', moduleId: PLATFORM.moduleName("./pages/home") },
-      { route: 'recipe', name: 'recipe', moduleId: PLATFORM.moduleName("./pages/recipe"), nav: true, title: 'Recipe' },
-      { route: 'new-recipe', name: 'new-recipe', moduleId: PLATFORM.moduleName("./pages/new-recipe"), nav: true, title: 'New Recipe' },
+      { route: 'recipe', name: 'recipe', moduleId: PLATFORM.moduleName("./pages/recipe-list"), nav: true, title: 'Recept' },
+      { route: 'new-recipe', name: 'new-recipe', moduleId: PLATFORM.moduleName("./pages/new-recipe"), nav: true, title: 'Nytt Recept' },
     ]);
   }
 }
